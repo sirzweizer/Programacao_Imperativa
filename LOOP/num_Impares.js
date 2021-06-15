5ca274bdf33fa89558e669318a3b7f29795db942
patrickr <patrickrodriguesseo@gmail.com>
2021-06-15 13:24:14 -0400

    Resolvendo micro desafio de LOOP

---
+++ LOOP/num_Impares.js
@@ -0,0 +1,5 @@
+for (let nImpar = 1; nImpar <=10; nImpar++){
+    if (nImpar%2==1){
+        console.log ("Repetição:", nImpar);
+    }    
+}
---
+++ LOOP/tabuada.js
@@ -0,0 +1,6 @@
+console.log("Tabuada do 3:");
+
+for (let i = 0; i<=10; i++){
+    
+    console.log ("3 x",i,"=",(3*i));
+}
