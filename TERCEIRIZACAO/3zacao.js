17f6fa54ca898440fe7c7098e4f7db2326dd19bb
patrickr <patrickrodriguesseo@gmail.com>
2021-06-18 09:52:07 -0400

    Resolucao exercicio callback - Patrick Rodrigues

---
+++ TERCEIRIZACAO/3zacao.js
@@ -0,0 +1,15 @@
+
+function andar (){
+    console.log('O carro está andando');
+}
+
+function parar (){
+    console.log('O carro parou');
+}
+
+function acaoCarro (){
+    andar();
+    parar();
+};
+
+acaoCarro();
