7ff6cdeeebfe59a5e5dce30f4da53b601a8fd199
patrickr <patrickrodriguesseo@gmail.com>
2021-06-22 10:07:25 -0400

    Resolucao micro desafio Meu Cardapio - PR

---
+++ CARDAPIO/exibe-cardapio.js
@@ -0,0 +1,9 @@
+let cardapio = ['Bife à milanesa', 'Lasanha', 'Baião de 2', 'Frango à passarinho'];
+
+function entrada() {
+    console.log('Seja Bem-Vindo(a) ao Restaurante Comida Caseira! Os pratos de hoje são: ' 
+    + cardapio.join(", ")+".");
+    
+}
+
+entrada();
